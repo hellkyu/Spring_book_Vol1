b6601815ca0229c935aef258e752eda932e397df
newkyu <newkyu12@gmail.com>
2018-02-08 13:41:47 +0900

    독립시킨 DB 연결 기능인 SimpleConnectionMaker

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -4,9 +4,15 @@ import java.sql.*;
 
 import springbook.user.domain.User;
 
-public abstract class UserDao {
+public class UserDao {
+	private SimpleConnectionMaker simpleConnectionMaker;
+	
+	public UserDao() {
+		simpleConnectionMaker = new SimpleConnectionMaker();
+	}
+	
 	public void add(User user) throws ClassNotFoundException, SQLException{
-		Connection c = getConnection();
+		Connection c = simpleConnectionMaker.makeNewConnection();
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
@@ -19,7 +25,7 @@ public abstract class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException{
-		Connection c = getConnection();
+		Connection c = simpleConnectionMaker.makeNewConnection();
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
@@ -35,23 +41,5 @@ public abstract class UserDao {
 		c.close();
 		
 		return user;
-	}
-	
-	public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-}
-
-class NUserDao extends UserDao{
-	@Override
-	public Connection getConnection() throws ClassNotFoundException, SQLException {
-		// N荤 DB connection 积己 内靛
-		return null;
-	}
-}
-
-class DUserDao extends UserDao{
-	@Override
-	public Connection getConnection() throws ClassNotFoundException, SQLException {
-		//D荤 DB connection 积己内靛
-		return null;
 	}
 }
