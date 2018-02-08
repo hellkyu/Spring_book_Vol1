99e80c723a922a379dad9054e3b2be077811b1c2
newkyu <newkyu12@gmail.com>
2018-02-08 18:06:47 +0900

    관계설정 책임의 분리

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.sql.*;
 
+import springbook.user.dao.ConnectionMaker;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 public class Main {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		UserDao dao = new UserDao();
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+		
+		UserDao dao = new UserDao(connectionMaker);
 		
 		User user = new User();
 		user.setId("whiteship");
--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -7,8 +7,8 @@ import springbook.user.domain.User;
 public class UserDao {
 	private ConnectionMaker connectionMaker;
 	
-	public UserDao() {
-		connectionMaker = new DConnectionMaker();
+	public UserDao(ConnectionMaker connectionMaker) {
+		this.connectionMaker = connectionMaker;
 	}
 	
 	public void add(User user) throws ClassNotFoundException, SQLException{
