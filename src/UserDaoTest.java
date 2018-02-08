088f0acc123911123cd9e1852ad6e22b8c704b9a
newkyu <newkyu12@gmail.com>
2018-02-08 18:12:22 +0900

    오브젝트 팩토리

--- src/UserDaoTest.java
+++ src/UserDaoTest.java
@@ -1,14 +1,12 @@
 import java.sql.*;
 
-import springbook.user.dao.ConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
-public class Main {
+public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		
-		UserDao dao = new UserDao(connectionMaker);
+		UserDao dao = new DaoFactory().userDao();
 		
 		User user = new User();
 		user.setId("whiteship");
