d7aaaa787042ed8f6320da6b7621819af8fca09f
newkyu <newkyu12@gmail.com>
2018-02-10 17:55:34 +0900

    테스트의 효율적인 수행과 결과 관리

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import org.junit.runner.JUnitCore;
+
+public class Main {
+	public static void main(String args[]) {
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
+	}
+}
--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,36 +1,36 @@
+package springbook.user.dao;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.*;
+
+import org.junit.Test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-import springbook.user.dao.DaoFactory;
-import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 public class UserDaoTest {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+	
+	@Test
+	public void addAndGet() throws SQLException{ //JUnit 테스트 메소드는 반드시 public 선언
 		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
+		user.setId("gyumee");
+		user.setName("박성철");
+		user.setPassword("springno1");
 		
 		dao.add(user);
-		
-		System.out.println(user.getId() + " 등록 성공");
+	
 		
 		User user2 = dao.get(user.getId());
 		
-		if(!user.getName().equals(user2.getName())) {
-			System.out.println("테스트 실패 (name)");
-		} else if(!user.getPassword().equals(user2.getPassword())) {
-			System.out.println("테스트 실패 (password)");
-		} else {
-			System.out.println("조회 테스트 성공");
-		}
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
 	}
 }
