7f2abdc9d52803cc7414740b3680c4523e8973a8
newkyu <newkyu12@gmail.com>
2018-02-17 20:49:30 +0900

    익명 내부 클래스

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -34,9 +34,19 @@ public class UserDao {
 		}
 	}
 
-	public void add(User user) throws SQLException{
-		StatementStrategy st = new AddStatement(user);
-		jdbcContextWithStatementStrategy(st);
+	public void add(final User user) throws SQLException{
+		jdbcContextWithStatementStrategy(
+				new StatementStrategy() {
+					public PreparedStatement makePreparedStatement(Connection c) throws SQLException{
+						PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+						ps.setString(1, user.getId());
+						ps.setString(2, user.getName());
+						ps.setString(3, user.getPassword());
+						
+						return ps;
+					}
+				}
+		);
 	}
 	
 	public User get(String id) throws SQLException{
@@ -64,8 +74,12 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStrategy(st);
+		jdbcContextWithStatementStrategy(new StatementStrategy() {
+			@Override
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				return c.prepareStatement("delete from users");
+			}
+		});
 	}
 	
 	public int getCount() throws SQLException {
