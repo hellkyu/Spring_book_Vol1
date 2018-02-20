29253a673703715e21af40fc3fcd0279a3232dca
newkyu <newkyu12@gmail.com>
2018-02-21 04:43:29 +0900

    다이내믹 프록시 적용

--- src/springbook/learningtest/jdk/HelloTest.java
+++ src/springbook/learningtest/jdk/HelloTest.java
@@ -2,6 +2,8 @@ package springbook.learningtest.jdk;
 
 import org.junit.Test;
 
+import java.lang.reflect.Proxy;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,7 +16,13 @@ public class HelloTest {
         assertThat(hello.sayHi("Toby"), is("Hi Toby"));
         assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
 
-        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),    //다이내믹 프록시가 정의되는 클래스로더 지정
+                new Class[] {Hello.class},      //다이내믹 프록시가 구현할 인터페이스 배열
+                new UppercaseHandler(new HelloTarget()));   // 부가기능과 위임 관련 코드를 담는 InvocationHandler 구현 오브젝트
+
+
+
         assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
         assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
         assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
